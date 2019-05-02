0001ba2d80e2afa8ce91ac04933789291b9e7661
OctopiWithHats <sdgranoff@gmail.com>
2019-05-02 13:39:10 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/authors', to: 'authors#index'
+  get 'authors/new', to: 'authors#new'
+  post '/authors', to: 'authors#create'
+  get 'authors/:id', to 'authors#show', as: 'author'
+  get 'authors/:id/edit', to: 'authors#edit'
+  patch '/authors', to: 'authors#update'
 end
